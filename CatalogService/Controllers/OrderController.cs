1f264766e472f8987d42ba65034345769bf0c75a
veeroteen <73069405+veeroteen@users.noreply.github.com>
2021-05-15 23:49:22 +0700

    гитигнор

--- CatalogService/Controllers/OrderController.cs
+++ CatalogService/Controllers/OrderController.cs
@@ -1,36 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using CatalogService.Models;
-using CatalogService.Data;
 
 namespace CatalogService.Controllers
 {
-    
-    [ApiController]
-    [Route("[controller]")]
-    public class OrderController : ControllerBase
+    public class OrderController : Controller
     {
-        public OrderController() { }
-
-
-
-
-        [HttpGet]
-        public IEnumerable<int> Get(string s)
+        public IActionResult Index()
         {
-
-            return Enumerable.Range(1, 1).Select(index => 1).ToArray();
+            return View();
         }
-
-
-
-
-
-        
     }
 }
